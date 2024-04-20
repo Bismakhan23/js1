5d11c88e7db4e00845b8de928db4052fe4fc8df9
Bismakhan23 <bismafarhan576@gmail.com>
2024-04-21 00:38:48 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+alert("Error! Please enter valid password");
+alert("Welcome to js land...                                                                        HappyCoding!     ");
+alert("Welcome to JS Land..");
+alert("Prevent the page from creating addional dialog");
+alert("Hello.. i can run JS through my web browser's console");
